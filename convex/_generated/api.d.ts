ca9e3f6ea82a6ed888119dec1ac1e215f82efa18
Bluva <bluvadigital@gmail.com>
2025-11-03 18:26:58 +0200

    updated build

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,45 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as drafts from "../drafts.js";
+import type * as helpers from "../helpers.js";
+import type * as rooms from "../rooms.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  drafts: typeof drafts;
+  helpers: typeof helpers;
+  rooms: typeof rooms;
+}>;
+declare const fullApiWithMounts: typeof fullApi;
+
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
